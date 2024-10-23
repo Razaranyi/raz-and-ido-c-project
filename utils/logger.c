de695984036b49eebf5c40beaab7db51f580c820
Raz Aranyi <razaranyi@gmail.com>
2024-10-23 10:03:25 +0000

    logger .c & .h

---
+++ utils/logger.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include "logger.h"
+
+static LogLevel log_level;
+
+void print_log_by_level(LogLevel level, const char* prefix, const char *message, int line) {
+    char log_res[1024];
+
+    /* Check if the log level is high enough to log the message */
+    if (level < log_level) {
+        return;
+    }
+
+    /* Format the log message and store it in log_res */
+    sprintf(log_res, "%s%s (line: %d)\n", prefix, message, line);
+
+    /* Print the log message to stdout */
+    printf("%s", log_res);
+}
+
+void debug(const char* message, int line) {
+    print_log_by_level(DEBUG, "[DEBUG] ", message, line);
+}
+
+void warn(const char* message, int line) {
+    print_log_by_level(WARN, "[WARN] ", message, line);
+}
+
+void error(const char* message, int line) {
+    print_log_by_level(ERROR, "[ERROR] ", message, line);
+}
+
+void fatal(const char* message, int line) {
+    print_log_by_level(FATAL, "[FATAL] ", message, line);
+}
---
+++ utils/logger.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/* Log level Enum */
+typedef  enum {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL
+} LogLevel;
+
+
+/*print log in level debug*/
+void debug(char* message, int line);
+
+/*print log in level info*/
+void info(char* message, int line);
+
+/*print log in level warning*/
+void warn(char* message, int line);
+
+/*print log in level error*/
+void error(char* message, int line);
+
+/*print log in level critical*/
+void critical(char* message, int line);
