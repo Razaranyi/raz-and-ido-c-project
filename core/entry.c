036237435ac1e7b2ff52e444246640f296fe5785
Raz Aranyi <razarnyi@gmail,com>
2024-12-20 18:23:15 +0000

    intellij stuck

---
+++ core/entry.c
@@ -0,0 +1,5 @@
+//
+// Created by root on 12/20/24.
+//
+
+#include "entry.h"
---
+++ core/entry.h
@@ -0,0 +1,8 @@
+//
+// Created by root on 12/20/24.
+//
+
+#ifndef RAZ_AND_IDO_C_PROJECT_ENTRY_H
+#define RAZ_AND_IDO_C_PROJECT_ENTRY_H
+
+#endif //RAZ_AND_IDO_C_PROJECT_ENTRY_H
